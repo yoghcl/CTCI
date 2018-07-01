9176d68cf514e6119d8a536f4315009cc6dd1afb
Yogesh Sharma <yosharma@microsoft.com>
2018-07-01 18:30:42 +0530

    added the naive implementation of String permutation problem

---
+++ Problems.Tests/ArraysAndStrings/StringPermutationTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems.ArraysAndStrings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.ArraysAndStrings.Tests
+{
+    [TestClass()]
+    public class StringPermutationTests
+    {
+        [TestMethod()]
+        public void IsPermutationNaiveMethodTest_WhenStringsArePermuationOfAnother()
+        {
+            var input1 = "God is great";
+            var input2 = "odg si taerG";
+
+            Assert.AreEqual(true, new StringPermutation().IsPermutationNaiveMethod(input1, input2));
+        }
+
+        [TestMethod()]
+        public void IsPermutationNaiveMethodTest_WhenStringsAreNotPermuationOfAnother()
+        {
+            var input1 = "God is great";
+            var input2 = "odg si taega";
+
+            Assert.AreEqual(false, new StringPermutation().IsPermutationNaiveMethod(input1, input2));
+        }
+
+        [TestMethod()]
+        public void IsPermutationNaiveMethodTest_WhenStringsAreUnqualLength()
+        {
+            var input1 = "God is great";
+            var input2 = "odg sitaerg";
+
+            Assert.AreEqual(false, new StringPermutation().IsPermutationNaiveMethod(input1, input2));
+        }
+    }
+}
---
+++ Problems/ArraysAndStrings/StringPermutation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.ArraysAndStrings
+{
+    public class StringPermutation
+    {
+        //given two strings, check whether one is permutation of another or not.
+        public bool IsPermutationNaiveMethod(string input1, string input2)
+        {
+            if (input1.Length != input2.Length) return false;
+
+            if (Sort(input1).Equals(Sort(input2)))
+            {
+                Trace.WriteLine(input1);
+                Trace.WriteLine(input2);
+                return true;
+            }
+            else
+                return false;
+        }
+
+        private string Sort(string s)
+        {
+            var characters = s.ToCharArray();
+            Array.Sort(characters);
+            return new string(characters);
+        }
+    }
+}
