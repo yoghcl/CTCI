3742110769bc0f6f29413ed3451d8172c4941b60
Yogesh Sharma <yosharma@microsoft.com>
2018-07-02 09:56:15 +0530

    Problem for Url Decoding

---
+++ Problems/ArraysAndStrings/UrlDecoding.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.ArraysAndStrings
+{
+    public class UrlDecoding
+    {
+        public string Decode(char[] input, int stringLength)
+        {
+            var spaceCount = 0;
+
+            //counting the spaces
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == ' ')
+                    spaceCount++;
+            }
+
+            var index = stringLength + spaceCount * 2;
+            if (stringLength < input.Length)
+                input[stringLength] = '\0';
+
+
+        }
+    }
+}
