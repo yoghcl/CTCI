c330c95463035c6f21ad44b9c76a07e1319bc98a
Yogesh Sharma <yosharma@microsoft.com>
2019-06-08 09:46:27 +0530

    Added the Null and Bolts problem

---
+++ Problems.Tests/Misc/NutsAndBoltsProblemTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems.Misc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Misc.Tests
+{
+    [TestClass()]
+    public class NutsAndBoltsProblemTests
+    {
+        [TestMethod()]
+        public void MatchPairsTest()
+        {
+            // Nuts and bolts are represented as array of characters 
+            char[] nuts =  { '2', '5', '3', '6', '4', '1' };
+            char[] bolts = { '1', '6', '2', '3', '4', '5' };
+
+            var nutsAndBolts = new NutsAndBoltsProblem();
+
+            // Method based on quick sort which matches nuts and bolts 
+            nutsAndBolts.MatchPairs(nuts, bolts, 5, 0);
+
+            Console.WriteLine("Matched nuts and bolts are : ");
+            PrintArray(nuts);
+            PrintArray(bolts);
+        }
+
+        private void PrintArray(char[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Problems/Misc/NutsAndBoltsProblem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Misc
+{
+    public class NutsAndBoltsProblem
+    {
+        public void MatchPairs(char[] nuts, char[] bolts, int high, int low)
+        {
+            if (low < high)
+            {
+                int pivot = Parition(nuts, high, low, bolts[high]);
+                Parition(bolts, high, low, nuts[pivot]);
+                MatchPairs(nuts, bolts, pivot - 1, low);
+                MatchPairs(nuts, bolts, high, pivot + 1);
+            }
+        }
+
+        private static int Parition(char[] array, int high, int low, char pivot)
+        {
+            int i = low;
+            char temp1, temp2;
+
+            for (int j = low; j < high; j++) 
+            {
+                if (array[j] < pivot)
+                {
+                    temp1 = array[i];
+                    array[i] = array[j];
+                    array[j] = temp1;
+                    i++;
+                }
+                else if (array[j] == pivot)
+                {
+                    temp1 = array[j];
+                    array[j] = array[high];
+                    array[high] = temp1;
+                    j--;
+                }
+            }
+
+            temp2 = array[i];
+            array[i] = array[high];
+            array[high] = temp2;
+
+            return i;
+        }
+    }
+}
