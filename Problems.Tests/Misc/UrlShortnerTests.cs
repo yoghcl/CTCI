d43209de15089e65f9a153ab97e2ac5461a68a12
Yogesh Sharma <yosharma@microsoft.com>
2019-06-11 19:21:48 +0530

    Added the url shortner problem

---
+++ Problems.Tests/Misc/UrlShortnerTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems.Misc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Misc.Tests
+{
+    [TestClass()]
+    public class UrlShortnerTests
+    {
+        [TestMethod()]
+        public void IdToShortUrlTest()
+        {
+            var id = 12345;
+            var urlShortner = new UrlShortner();
+            var result = urlShortner.IdToShortUrl(id);
+
+            Assert.AreEqual("dnh", result);
+        }
+    }
+}
---
+++ Problems/Misc/UrlShortner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Misc
+{
+    public class UrlShortner
+    {
+        public string IdToShortUrl(int n)
+        {
+            string allowchars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+            char[] map =
+                allowchars.ToCharArray();
+
+            StringBuilder shortUrl = new StringBuilder();
+
+            while (n > 0)
+            {
+                shortUrl.Append(map[n % 62]);
+                n /= 62;
+            }
+
+            return Reverse(shortUrl.ToString());
+        }
+
+        private string Reverse(string value)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = value.Length - 1; i >= 0; i--)
+            {
+                sb.Append(value[i]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
