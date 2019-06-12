a1ff1dd03cf155aba56e5e5967cf1717ae9f0916
Yogesh Sharma <yosharma@microsoft.com>
2019-06-12 23:00:09 +0530

    Added the StringPermutations program.

---
+++ Problems.Tests/ArraysAndStrings/StringPermutationsTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems.ArraysAndStrings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.ArraysAndStrings.Tests
+{
+    [TestClass()]
+    public class StringPermutationsTests
+    {
+        [TestMethod()]
+        public void GetAllPermutationsTest()
+        {
+            var stringPermutations = new StringPermutations();
+            var result = stringPermutations.GetAllPermutations("ABC");
+            Assert.AreEqual("ABC ACB BAC BCA CBA CAB", result);
+        }
+
+        [TestMethod()]
+        public void GetAllPermutationsTest_WithFourCharacters()
+        {
+            var stringPermutations = new StringPermutations();
+            var result = stringPermutations.GetAllPermutations("ABCD");
+            Assert.AreEqual("ABCD ABDC ACBD ACDB ADCB ADBC BACD BADC BCAD BCDA BDCA BDAC CBAD CBDA CABD CADB CDAB CDBA DBCA DBAC DCBA DCAB DACB DABC", result);
+        }
+
+        [TestMethod()]
+        public void GetAllPermutationsTest_WithFiveCharacters()
+        {
+            var stringPermutations = new StringPermutations();
+            var result = stringPermutations.GetAllPermutations("ABCDE");
+            Assert.AreEqual("ABCDE ABCED ABDCE ABDEC ABEDC ABECD ACBDE ACBED ACDBE ACDEB ACEDB ACEBD ADCBE ADCEB ADBCE ADBEC ADEBC ADECB AECDB AECBD AEDCB AEDBC AEBDC AEBCD BACDE BACED BADCE BADEC BAEDC BAECD BCADE BCAED BCDAE BCDEA BCEDA BCEAD BDCAE BDCEA BDACE BDAEC BDEAC BDECA BECDA BECAD BEDCA BEDAC BEADC BEACD CBADE CBAED CBDAE CBDEA CBEDA CBEAD CABDE CABED CADBE CADEB CAEDB CAEBD CDABE CDAEB CDBAE CDBEA CDEBA CDEAB CEADB CEABD CEDAB CEDBA CEBDA CEBAD DBCAE DBCEA DBACE DBAEC DBEAC DBECA DCBAE DCBEA DCABE DCAEB DCEAB DCEBA DACBE DACEB DABCE DABEC DAEBC DAECB DECAB DECBA DEACB DEABC DEBAC DEBCA EBCDA EBCAD EBDCA EBDAC EBADC EBACD ECBDA ECBAD ECDBA ECDAB ECADB ECABD EDCBA EDCAB EDBCA EDBAC EDABC EDACB EACDB EACBD EADCB EADBC EABDC EABCD", result);
+        }
+    }
+}
---
+++ Problems/ArraysAndStrings/StringPermutations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.ArraysAndStrings
+{
+    public class StringPermutations
+    {
+        public string GetAllPermutations(string input)
+        {
+            var permutations = new List<string>();
+            Permute(input.ToCharArray(), 0, input.Length - 1, permutations);
+            return string.Join(" ", permutations);
+        }
+
+        private void Permute(char[] input, int left, int right, IList<string> permutations)
+        {
+            if (left != right)
+            {
+                for (int i = left; i <= right; i++)
+                {
+                    Swap(ref input[left], ref input[i]);
+                    Permute(input, left + 1, right, permutations);
+                    Swap(ref input[left], ref input[i]);
+                }
+            }
+            else
+            {
+                permutations.Add(new string(input));
+            }
+        }
+
+        private void Swap(ref char v1, ref char v2)
+        {
+            var temp = v1;
+            v1 = v2;
+            v2 = temp;
+        }
+    }
+}
