e936928cd3e0a019c276a6740010cc39de43b0d0
Yogesh Sharma <yosharma@microsoft.com>
2019-06-26 20:15:07 +0530

    Added the KCounts problem

---
+++ Problems.Tests/Searching/KCountsTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems.Searching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Searching.Tests
+{
+    [TestClass()]
+    public class KCountsTests
+    {
+        [TestMethod()]
+        public void CountsTest_ShouldReturn_3()
+        {
+            var numbers = new int[] { 0, 1, 2, 3, 5, 5, 5, 6, 8, 19 };
+            var countingNumber = 5;
+
+            var result = new KCounts().Counts(numbers, countingNumber);
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod()]
+        public void CountsTest_ShouldReturn_1()
+        {
+            var numbers = new int[] { 0, 1, 2, 3, 5, 5, 5, 6, 8, 19 };
+            var countingNumber = 19;
+
+            var result = new KCounts().Counts(numbers, countingNumber);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod()]
+        public void CountsTest_ShouldReturn_Negavtive_1()
+        {
+            var numbers = new int[] { 0, 1, 2, 3, 5, 5, 5, 6, 8, 19 };
+            var countingNumber = 100;
+
+            var result = new KCounts().Counts(numbers, countingNumber);
+            Assert.AreEqual(-1, result);
+        }
+    }
+}
---
+++ Problems/Searching/KCounts.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Searching
+{
+    public class KCounts
+    {
+        public int Counts(int[] numbers, int countingNumber)
+        {
+            var leftMostIndex = LeftIndex(numbers, 0, numbers.Length - 1, countingNumber);
+            var rightMostIndex = RightIndex(numbers, numbers.Length, 0, numbers.Length - 1, countingNumber);
+
+            return (leftMostIndex != -1) && (rightMostIndex != -1) ? rightMostIndex - leftMostIndex + 1 : -1;
+        }
+
+        private int LeftIndex(int[] numbers, int low, int high, int numberTofind)
+        {
+            if (low > high) return -1;
+
+            var mid = low + (high - low) / 2;
+
+            if ((numbers[mid] == numberTofind) && (mid == 0 || numbers[mid - 1] != numberTofind))
+            {
+                return mid;
+            }
+
+            if (numbers[mid] >= numberTofind)
+                return LeftIndex(numbers, low, mid - 1, numberTofind);
+            else
+                return LeftIndex(numbers, mid + 1, high, numberTofind);
+        }
+
+        private int RightIndex(int[] numbers, int length, int low, int high, int numberTofind)
+        {
+            if (low > high) return -1;
+
+            var mid = low + (high - low) / 2;
+
+            if ((numbers[mid] == numberTofind) && (mid == length - 1 || numbers[mid + 1] != numberTofind))
+            {
+                return mid;
+            }
+
+            if (numbers[mid] > numberTofind)
+                return RightIndex(numbers, length, low, mid - 1, numberTofind);
+            else
+                return RightIndex(numbers, length, mid + 1, high, numberTofind);
+        }
+    }
+}
