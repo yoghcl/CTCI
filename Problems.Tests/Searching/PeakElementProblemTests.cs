b6e3bd685424ae55ec41bc2dd362e9c97c826672
Yogesh Sharma <yosharma@microsoft.com>
2019-06-28 09:41:52 +0530

    Peak Element problem

---
+++ Problems.Tests/Searching/PeakElementProblemTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems.Searching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Searching.Tests
+{
+    [TestClass()]
+    public class PeakElementProblemTests
+    {
+        [TestMethod()]
+        public void PeakTest_Should_Return_5()
+        {
+            var numbers = new int[] { 1, 2, 3, 5, 4, 2, 1 };
+            Assert.AreEqual(5, new PeakElementProblem().Peak(numbers));
+        }
+
+        [TestMethod()]
+        public void PeakTest_Should_Return_3()
+        {
+            var numbers = new int[] { 1, 2, 3 };
+            Assert.AreEqual(3, new PeakElementProblem().Peak(numbers));
+        }
+
+        [TestMethod()]
+        public void PeakTest_Should_Return_4()
+        {
+            var numbers = new int[] { 4, 2, 3 };
+            Assert.AreEqual(4, new PeakElementProblem().Peak(numbers));
+        }
+
+        [TestMethod()]
+        public void PeakTest_Should_Return_7()
+        {
+            var numbers = new int[] { 7, 3, 2 };
+            Assert.AreEqual(7, new PeakElementProblem().Peak(numbers));
+        }
+    }
+}
---
+++ Problems/Searching/PeakElementProblem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Searching
+{
+    public class PeakElementProblem
+    {
+        public int Peak(int[] numbers)
+        {
+            return Peak(numbers, 0, numbers.Length - 1, numbers.Length);
+        }
+
+        private int Peak(int[] numbers, int low, int high, int size)
+        {
+            if (low > high) return -1;
+
+            var mid = low + (high - low) / 2;
+
+            // middle element is greater then its two neighbours
+            if ((mid == 0 || numbers[mid - 1] <= numbers[mid]) && (mid == size - 1 || numbers[mid] > numbers[mid + 1]))
+                return numbers[mid];
+
+            // If middle element is not peak and its left neighbour is greater  
+            // than it, then left half must have a peak element 
+            else if (mid > 0 && numbers[mid - 1] > numbers[mid])
+                return Peak(numbers, low, mid - 1, size);
+
+            // If middle element is not peak and its right neighbour is greater 
+            // than it, then right half must have a peak element 
+            else
+                return Peak(numbers, (mid + 1), high, size);
+        }
+    }
+}
