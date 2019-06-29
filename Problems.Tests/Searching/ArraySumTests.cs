82e595461c95e56944651d4a747b43a04136c5ac
Yogesh Sharma <yosharma@microsoft.com>
2019-06-29 23:41:50 +0530

    Added the ArraySum (Triplets) problem.

---
+++ Problems.Tests/Searching/ArraySumTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems.Searching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Searching.Tests
+{
+    [TestClass()]
+    public class ArraySumTests
+    {
+        [TestMethod()]
+        public void IsFoundTest()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            Assert.AreEqual(true, new ArraySum().IsFound(numbers, 21));
+        }
+    }
+}
---
+++ Problems/Searching/ArraySum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Searching
+{
+    public class ArraySum
+    {
+        public bool IsFound(int [] numbers, int sum)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (IsPair(numbers, i + 1, numbers.Length - 1, sum - numbers[i]))
+                    return true;
+            }
+
+            return false;
+        }
+
+        private bool IsPair(int[] numbers, int start, int end, int sum)
+        {
+            while ( start < end)
+            {
+                if (numbers[start] + numbers[end] == sum)
+                    return true;
+                if (numbers[start] + numbers[end] > sum)
+                    end--;
+                else
+                    start++;
+            }
+
+            return false;
+        }
+    }
+}
