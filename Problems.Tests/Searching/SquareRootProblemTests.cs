c88a44c9f4e7cecd3bf188cfcd1f47b398dafd7b
Yogesh Sharma <yosharma@microsoft.com>
2019-06-29 13:28:01 +0530

    Added the square root problem

---
+++ Problems.Tests/Searching/SquareRootProblemTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems.Searching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Searching.Tests
+{
+    [TestClass()]
+    public class SquareRootProblemTests
+    {
+        [TestMethod()]
+        public void Should_Return_4()
+        {
+            var n = 16;
+            Assert.AreEqual(4, new SquareRootProblem().Sqrt(n));
+        }
+
+        [TestMethod()]
+        public void Should_Return_3()
+        {
+            var n = 15;
+            Assert.AreEqual(3, new SquareRootProblem().Sqrt(n));
+        }
+
+    }
+}
---
+++ Problems/Searching/SquareRootProblem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Searching
+{
+    public class SquareRootProblem
+    {
+        public int Sqrt(int n)
+        {
+            if (n == 0 || n == 1)
+                return n;
+
+            var start = 1;
+            var end = n;
+            var ans = 0;
+
+            while (start <= end)
+            {
+                var mid = (start + end) / 2;
+
+                if (mid * mid == n)
+                    return mid;
+
+                if (mid * mid < n)
+                {
+                    start = mid + 1;
+                    ans = mid;
+                }
+                else
+                    end = mid - 1;
+            }
+
+            return ans;
+        }
+    }
+}
